dca2b6bff8c0e88719f5b996b7047e992b36e44e
MatthiasFloru <matthias.floru@kellogg.northwestern.edu>
2022-05-20 21:26:09 +0000

    finished in class

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,18 +9,28 @@ class SessionsController < ApplicationController
         if @user
             # Step 1a: if yes, check the password (go to step 2)
             # Step 2: check the password to check if it matches the users password
-            if @user["password"]==params["password"]
+            if BCrypt::Password.new(@user["password"])==params["password"]
                 # Step 2a: if yes, go to the companies page
+                session["user_id"] = @user["id"]
+                flash["notice"] = "You're in!"
                 redirect_to "/companies"
             else
                 # Step 2b: if no, go back to the login page
+                flash["notice"] = "Incorrect credentials"
                 redirect_to "/sessions/new"
             end
         else
             # Step 1b: if no, go back to the login page
+            flash["notice"] = "Incorrect credentials"
             redirect_to "/sessions/new"
         end
 
+    end
+
+    def destroy
+        flash["See ya later!"]
+        session["user_id"] = nil
+        redirect_to "/sessions/new"
     end
 
 end
